15345eec96dd933a690b058b45163349e5bd3c96
Adrian Lee <adrianlee44@gmail.com>
2011-07-21 01:07:23 -0700

    Basic framework for the site. Currently added mark's code as first commit.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Movie Trend</title>
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/grid.css">
+		<link rel="stylesheet" href="css/style.css">
+		
+		<script type="text/javascript" src="js/jquery-1.6.2.min.js"></script>
+		<script type="text/javascript" src="js/index.js"></script>
+	</head>
+	<body>
+		<input type="text" id="search"/ value="the undefeated"><button id="go">Go!</button>
+		<h1 id="title">Tweets about <span class='movieTitle'>Star Wars</span></h1>
+		<ul class='entries'>
+		</ul>
+		<a href="#" id="more">next</a>
+		<div id="locations"></div>
+	</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,42 @@
+var baseUrl = 'http://search.twitter.com/search.json?callback=?&q=';
+var searchBase = 'http://search.twitter.com/search.json';
+$(document).ready(function() {
+    $('#title').hide();
+    $('#more').hide();
+    $('#search').width(200);
+    $('#go').click(function() {
+        var query = $('#search').val();
+        console.log();
+        $('.entries').html('');
+        search(baseUrl + encodeURI(query));
+        $('#title').show().children('.movieTitle').html(query);
+        $('#locations').append('<h1>Locations for ' + query + '</h2>');
+    });
+    $('#go').click();
+});
+
+function search(url) {
+    $.getJSON(url, function(json) {
+        var locations = Array();
+        $.each(json.results, function(i, tweet) {
+            $('.entries').append('<li class="entry"><img src="' + tweet.profile_image_url + '" width="48" height="48" />' + tweet.text + '</li>');
+            if (tweet.geo != null) {
+                locations.push(tweet.geo.coordinates);
+            }
+        });
+        $('#locations').append('<p>' + locations + '</p>');
+        if (json.next_page != null) {
+            console.log(json.next_page);
+            $('#more').show().click(function(e) {
+                e.preventDefault();
+                var params = json.next_page + '';
+                console.log(searchBase + params + '&callback=?');
+                search(searchBase + params + '&callback=?');
+
+            });
+        }
+        else {
+            $('#more').hide();
+        }
+    });
+}
